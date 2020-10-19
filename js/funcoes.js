6ff30dc6de7f6c63231f9b669eedbee6cfcc225c
Josiel Souza <josielsouza.dj@gmail.com>
2020-10-18 21:45:06 -0300

    corrigir js

--- js/funcoes.js
+++ js/funcoes.js
@@ -1,32 +1,34 @@
 function exibir_categoria(categoria) {
-  //Alerta categoria
-  let elementos = document.getElementsByClassName("container_intens");
-  console.log(elementos);
-  for (var i = 0; i < elementos.length; i++) {
-    console.log(elementos[i].id);
+    //Alerta categoria
+    let elementos = document.getElementsByClassName('container_intens');
+    console.log(elementos);
+    for (var i = 0; i < elementos.length; i++) {
+        console.log(elementos[i].id);
 
-    if (categoria == elementos[i].id) {
-      elementos[i].style = "display:block";
-    } else {
-      elementos[i].style = "display:none";
+        if (categoria == elementos[i].id) {
+            elementos[i].style = "display:block";
+        }
+        else {
+            elementos[i].style = "display:none";
+        }
     }
-  }
 }
 
-function exibir_todos() {
-  let elementos = document.getElementsByClassName("container_intens");
+let exibir_todos = () => {
+    let elementos = document.getElementsByClassName('container_intens');
 
-  for (var i = 0; i < elementos.length; i++) {
-    elementos[i].style = "display:block";
-  }
-}
+    for (var i = 0; i < elementos.length; i++) {
+
+        elementos[i].style = "display:block";
+    }
+};
 let destaque = (imagem) => {
-  console.log(imagem);
-  if (imagem.style.width === "120%") {
-    imagem.style.width = "100%";
-    imagem.style.height = "50%";
-  } else {
-    imagem.style.width = "120%";
-    imagem.style.height = "80%";
-  }
+    console.log(imagem);
+    if (imagem.style.height === "80%") {
+        imagem.style.height = "50%";
+        imagem.parentNode.style.width = "20%";
+    } else {
+        imagem.style.height = "80%";
+        imagem.parentNode.style.width = "30%";
+    }
 };
